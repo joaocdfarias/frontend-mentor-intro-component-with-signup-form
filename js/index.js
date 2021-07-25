cb27276d1c4a32127860f2527875bee48a109021
joaocdfarias <joaocdfariass@gmail.com>
2021-07-25 17:37:39 -0300

    javascript modification

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-const input = document.querySelectorAll('input')
-const at = '@'
+const inputs = document.querySelectorAll('input')
+const errorText = document.createElement('span')
 
-input.forEach((item) => {
+inputs.forEach((item) => {
   item.addEventListener('focusout', (event) => {
     if (!event.target.value) {
       item.classList.add('icon-error')
-      const errorText = document.createElement('span')
       item.parentNode.insertBefore(errorText, item.nextSibling)
       errorText.innerText = `${item.placeholder} cannot be empty`
       errorText.classList.add('error-message')
       return
+    } else {
+      item.classList.remove('icon-error')
+      errorText.remove()
     }
-    item.classList.remove('icon-error')
   })
 })
